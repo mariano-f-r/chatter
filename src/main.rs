23135614b9cefb7eb90bde902a861c7754af2fab
Mariano Rodriguez <amr001.us@proton.me>
2023-10-01 16:38:18 -0500

    Prepare for deploy to Shuttle.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use serde::Deserialize;
 use serde_json;
+use shuttle_runtime;
 use std::fs;
+use std::net;
 use std::rc::Rc;
-use ws::{listen, Error, ErrorKind, Handler, Handshake, Message, Request, Response, Result};
+use ws::{listen, Error, ErrorKind, Handler, Handshake, Message, Request, Response};
 
 #[derive(Deserialize, Debug)]
 struct ChatMessage {
@@ -11,13 +13,25 @@ struct ChatMessage {
     content: String,
 }
 
-struct Server {
+struct CustomService;
+
+#[shuttle_runtime::async_trait]
+impl shuttle_runtime::Service for CustomService {
+    async fn bind(mut self, addr: net::SocketAddr) -> Result<(), shuttle_runtime::Error> {
+        let users: Rc<Vec<String>> = Rc::new(Vec::<String>::new());
+
+        listen(addr, |output| ClientHandler { output, ip: None }).unwrap();
+        Ok(())
+    }
+}
+
+struct ClientHandler {
     output: ws::Sender,
     ip: Option<String>,
 }
 
-impl Handler for Server {
-    fn on_request(&mut self, req: &Request) -> Result<Response> {
+impl Handler for ClientHandler {
+    fn on_request(&mut self, req: &Request) -> Result<Response, ws::Error> {
         println!("{} request to {:?}", req.method(), req.resource());
         match req.resource() {
             "/ws" => Response::from_request(req),
@@ -38,7 +52,7 @@ impl Handler for Server {
             )),
         }
     }
-    fn on_message(&mut self, msg: Message) -> Result<()> {
+    fn on_message(&mut self, msg: Message) -> Result<(), ws::Error> {
         let msg_json = msg.as_text()?;
         let parsed_message: ChatMessage = match serde_json::from_str(msg_json) {
             Ok(chatmessage) => chatmessage,
@@ -49,14 +63,13 @@ impl Handler for Server {
                 ))
             }
         };
-        dbg!("{}", &parsed_message);
         let sendable_message = format!(
             "{} at {}: {}",
             parsed_message.username, parsed_message.time, parsed_message.content
         );
         self.output.broadcast(sendable_message)
     }
-    fn on_open(&mut self, shake: Handshake) -> Result<()> {
+    fn on_open(&mut self, shake: Handshake) -> Result<(), ws::Error> {
         let address = shake.remote_addr()?;
         self.output.broadcast(format!(
             "{} has joined the chat",
@@ -65,9 +78,7 @@ impl Handler for Server {
     }
 }
 
-fn main() {
-    let users: Rc<Vec<String>> = Rc::new(Vec::<String>::new());
-    let host = "192.168.137.33:8000";
-    println!("Running on http://{}", host);
-    listen(host, |output| Server { output, ip: None }).unwrap();
+#[shuttle_runtime::main]
+async fn init() -> Result<CustomService, shuttle_runtime::Error> {
+    Ok(CustomService)
 }
